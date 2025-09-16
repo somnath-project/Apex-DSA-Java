e7129fd546aa0840f1785950022cec69b82a98e8
somnath-project <somnath.ekhande99@gmail.com>
2025-09-16 07:44:45 +0530

    Added

---
+++ Demo/src/Loop.java
@@ -0,0 +1,24 @@
+public class Loop {
+    public static void main(String args[]){
+
+    
+
+    /*
+     * do while loop:
+     * syntax:
+     * do{
+     * 
+     * }while(condition);
+     *
+     */
+    int n=1;
+    do{
+        System.out.println(n);
+        break;
+    }while(n!=1);
+
+
+
+
+}
+}
